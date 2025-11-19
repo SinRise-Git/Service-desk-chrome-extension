4865013c5d046b8961af2918ffb4976132924985
SinRise-Git <bfu01.bb@gmail.com>
2025-11-19 15:35:42 +0100

    refactor layout and update work time display in index.html and name-script.js

--- public/script/name-script.js
+++ public/script/name-script.js
@@ -1,27 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let currentName = "";
+    let currentName = "unknown";
+    let totalMinutesDay = 0;
+    let totalMinutesMonth = 0;
+
     chrome.storage.local.get(['user_name'], function (result) {
         if (result.user_name) {
             currentName = result.user_name;
             document.getElementById("nameDisplay").innerText = currentName;
         }
     });
-
-    function getWorkMinutes() {
-        let totalMinutes = 0;
-        chrome.storage.local.get(['time_entries'], function (result) {
-            if (result.time_entries) {
-                for (const entry of result.time_entries) {
-                    if (entry.user === currentName && entry.time === `${(new Date()).getDate()}/${(new Date()).getMonth() + 1}/${(new Date()).getFullYear()}`) {
-                        totalMinutes += entry.minutes;
-                    }
+    
+    chrome.storage.local.get(['time_entries'], function (result) {
+        if (result.time_entries) {
+            for (const entry of result.time_entries) {
+                console.log('Checking entry:', entry);
+                if (entry.user === currentName && entry.time === `${(new Date()).getDate()}/${(new Date()).getMonth() + 1}/${(new Date()).getFullYear()}`) {
+                    totalMinutesDay += entry.minutes;
                 }
-                let hours = Math.floor(totalMinutes / 60);
-                let minutes = totalMinutes % 60
-                document.getElementById("workHours").innerText = hours;
-                document.getElementById("workMinutes").innerText = minutes;
+                if (entry.user === currentName && entry.time.includes(`${(new Date()).getMonth() + 1}/${(new Date()).getFullYear()}`)) {
+                    totalMinutesMonth += entry.minutes;
+                    console.log('Adding minutes for month:', entry.minutes, 'Total now:', totalMinutesMonth);
+                }
             }
-        });
-    }
-    getWorkMinutes()
+            document.getElementById("workHoursDay").innerText = Math.floor(totalMinutesDay / 60);
+            document.getElementById("workMinutesDay").innerText = totalMinutesDay % 60;
+            document.getElementById("workHoursMonth").innerText = Math.floor(totalMinutesMonth / 60);
+            document.getElementById("workMinutesMonth").innerText = totalMinutesMonth % 60;
+        }
+    });
 });
