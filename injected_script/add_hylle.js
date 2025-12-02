98a9f619d472743f9067eb667735a4237420a786
SinRise-Git <bfu01.bb@gmail.com>
2025-12-02 13:07:38 +0100

    improved script

--- injected_script/add_hylle.js
+++ injected_script/add_hylle.js
@@ -1,59 +1,50 @@
-function startObservingAddCopyButton() {
-    var observer = new MutationObserver((mutations, observer) => {
-        const resolutionDiv = document.getElementById('js-event-Resolution-1');
-        const hylleDiv = document.querySelector('.req_header_hylle');
-        const copyButton = document.getElementById('copyHylleButton');
-        if (resolutionDiv && hylleDiv && !copyButton && window.location.hash == '#resolution') {
-            observer.disconnect();
-            addCopyButton(hylleDiv);
-        }
-    })
-
-    observer.observe(document, {
-        childList: true,
-        subtree: true,
-    });
-
-    function addCopyButton(hylleDiv) {
-        const line = document.getElementById('s2id_woResolution_Id');
-        const lineParent = line.parentElement.parentElement;
-        const button = document.createElement('button');
-
-        button.innerText = 'Kopier hylle';
-        button.style.padding = '4.75px 10px 4.75px 10px';
-        button.style.color = 'white';
-        button.style.backgroundColor = 'transparent';
-        button.style.border = '1px solid #4e4e4e';
-        button.style.marginLeft = '10px';
-        button.style.cursor = 'pointer';
-        button.id = 'copyHylleButton';
-
-        lineParent.appendChild(button);
-
-        button.addEventListener('click', (e) => {
-            e.preventDefault();
-            const hylleText = hylleDiv.querySelectorAll('.mr5.sb.fl')[1].innerText.trim();
-            if (hylleText != 'Ukjent') {
-                chrome.storage.local.get(['end_text'], function (result) {
-                    const hylleTextRaw = result.end_text || 'Ligger i $sted$'
-                    const hylleTextCopy = hylleTextRaw.includes('$sted$') ? hylleTextRaw.replace('$sted$', hylleText) : `Ligger i ${hylleText}`;
-                    navigator.clipboard.writeText(hylleTextCopy);
-                })
+(() => {
+    if (window.location.href.includes('#resolution')) {
+        const observer = new MutationObserver((mutations, observer) => {
+            const resolutionDiv = document.getElementById('js-event-Resolution-1');
+            const hylleDiv = document.getElementById('req_header_hylle');
+            const copyButton = document.getElementById('copyHylleButton');
+            if (resolutionDiv && hylleDiv && !copyButton) {
+                addCopyButton(hylleDiv);
             }
+        })
+
+        observer.observe(document, {
+            childList: true,
+            subtree: true,
         });
+
+        function addCopyButton(hylleDiv) {
+            const line = document.getElementById('s2id_woResolution_Id');
+            const lineParent = line.parentElement.parentElement;
+            const button = document.createElement('button');
+
+            button.innerText = 'Kopier hylle';
+            button.style.padding = '4.75px 10px 4.75px 10px';
+            button.style.color = 'white';
+            button.style.backgroundColor = 'transparent';
+            button.style.border = '1px solid #4e4e4e';
+            button.style.marginLeft = '10px';
+            button.style.cursor = 'pointer';
+            button.style.textAlign = 'center';
+            button.id = 'copyHylleButton';
+            
+            lineParent.appendChild(button);
+
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+                const hylleText = hylleDiv.querySelectorAll('.mr5.sb.fl')[1].innerText.trim()
+
+                if (hylleText === 'Ukjent') {
+                    window.alert('Dette stedet er ikke registrert med hylle, legg det til i listen for å kunne kopiere det.');
+                } else {
+                    chrome.storage.local.get(['end_text'], function (result) {
+                        const hylleTextRaw = result.end_text?.trim() || 'Ligger i $sted$'
+                        const hylleTextCopy = hylleTextRaw.includes('$sted$') ? hylleTextRaw.replace('$sted$', hylleText) : `Ligger i ${hylleText}`;
+                        navigator.clipboard.writeText(hylleTextCopy);
+                    })
+                }
+            });
+        }
     }
-}
-
-var triggeredByClickAddHylle = false;
-document.addEventListener('click', function (event) {
-    triggeredByClickAddHylle = true
-    if (event.target.hasAttribute('href') && window.location.hash == '#resolution') {
-        console.log('CLICK detected for add hylle button');
-        startObservingAddCopyButton();
-    }
-});
-
-if (!triggeredByClickAddHylle) {
-    startObservingAddCopyButton();
-}
-
+})();
