4c11abaf855fc7f5447a29e19bc597b17538d431
SinRise-Git <bfu01.bb@gmail.com>
2025-12-16 15:29:59 +0100

    fix: update storage settings and script injection logic for improved functionality

--- background.js
+++ background.js
@@ -1,28 +1,86 @@
+const steder = {
+  "Nord": [
+    "Brannstasjonen",
+    "Haugland skole",
+    "Ravnagner sykehjem (Helsestasjon, Omsorgsbolig, Sone Midtre)",
+    "Ravnanger ungdomsskole",
+    "Tveit skole",
+    "Tveit barnehage",
+    "Hanøy skole",
+    "Hanøy barnehage",
+    "Ramsøy barnehage",
+    "Davanger skole",
+    "Træet skole",
+    "Fromereide eldresenter",
+    "Fauskanger barne- og ungdomsskole",
+  ],
+  "Sør": [
+    "Rådhuset",
+    "Senteret",
+    "Holmedalen PLO",
+    "Energigården",
+    "Kleppestø sykehjem",
+    "Fenring legesenter",
+    "Rasmussenhuset",
+    "Kleppestø barneskole",
+    "Kleppestø barnehage",
+    "Kleppestø ungdomsskole",
+    "Kleppe skole",
+    "Kleppestø senter",
+    "Kleppegrennd",
+    "Juvik boliger",
+    "IT-avdelingen",
+    "Fagopplæring i Askøy kommune",
+    "Askøy kulturskole",
+  ],
+  "Vest": [
+    "Strusshamn skole",
+    "Shoddien",
+    "Strusshamn helsestasjon",
+    "Flagget omsorgsbolig",
+    "Flagget arbeidslag",
+    "Follese skole",
+    "Hetlevik skole"
+  ],
+  "Øst": [
+    "Florvåg skole",
+    "Flimmerne",
+    "Florvåg barnehage",
+    "Florvåg helsestasjon",
+    "Bakavågen",
+    "Erdal barneskole",
+    "Erdal ungdomsskole",
+    "Furulyområdet",
+    "Strømssnes boliger",
+    "Hop skole",
+    "Ask barnehage"
+  ]
+};
+
+chrome.storage.local.set({ steder: steder });
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status !== 'complete') return;
-  if (!tab.url) return;
-
-  if (!tab.url.startsWith('https://servicedesk.askoy.kommune.no/')) {
-    return;
-  }
+  
+  const url = tab.url || tab.pendingUrl;
+  if (!url || !url.startsWith('https://servicedesk.askoy.kommune.no/')) return;
+  
+  const fileList = [
+    'injected_script/delete_time.js',
+    'injected_script/add_hylle.js',
+    'injected_script/time_counter.js'
+  ];
 
   chrome.storage.local.get(['show_hylle'], (result) => {
-    const show_hylle = (typeof result.show_hylle === 'boolean')
-      ? result.show_hylle
-      : true;
-
-    if (!show_hylle) return;
+    if (result.show_hylle) {
+      fileList.push('injected_script/inject_hylle.js');
+    }
 
     chrome.scripting.executeScript({
       target: { tabId },
-      files: [
-        'injected_script/inject_hylle.js',
-        'injected_script/delete_time.js',
-        'injected_script/add_hylle.js',
-        'injected_script/time_counter.js'
-      ]
+      files: fileList
     }).catch(err => {
-      console.error('Script injection failed:', err);
+      console.error('Script injection failed:', err, url);
     });
   });
 });
